4a1ee896a264f6fe973fcbe7acaf5a1fe0f73f6d
António Pedro Azevedo Ribeiro Cardoso <antoniopedro.cardoso.it@gmail.com>
2024-02-25 15:42:39 +0000

    Resolução do TPC3, desconsiderando a presença de números negativos.

--- TP2/README.md
+++ TP2/README.md
@@ -5,4 +5,4 @@
 # Capaz de converter apenas a Sintaxe Básica contida em [Markdown guide](https://www.markdownguide.org/cheat-sheet/)
 # O ficheiro que contém o programa que realiza essa tarefa denomina-se "tpc2.py".
 # Foi usado o ficheiro "Exemplo.md", dado que o mesmo contém exemplos de toda a sintaxe básica, tendo sido gerado pelo programa um ficheiro de nome "Exemplo.html", onde se observa que a conversão ocorre corretamente.
-# Todos os ficheiros mencionados encontram-se nesta pasta
+# Todos os ficheiros mencionados encontram-se nesta pasta.
---
+++ TP3/tpc3.py
@@ -0,0 +1,46 @@
+import re
+import sys
+
+def somador_on_off(inp):
+    with open(inp, 'rb') as f:
+        content = f.read()
+
+    somar = True
+    resultado = 0
+    i = 0
+
+    while i < len(content):
+        char = chr(content[i])
+
+        if re.match(r'O', char, re.IGNORECASE):
+            i += 1
+            if re.match(r'N', chr(content[i]), re.IGNORECASE):
+                somar = True
+                i += 1
+        elif re.match(r'F', char, re.IGNORECASE):
+            i += 1
+            if re.match(r'F', chr(content[i]), re.IGNORECASE):
+                somar = False
+                i += 1
+        elif char == '=':
+            print(resultado)
+            i += 1
+        elif re.match(r'\d', char) and somar:
+            resultado_aux = 0
+            while i < len(content) and re.match(r'\d', chr(content[i])):
+                resultado_aux *= 10
+                resultado_aux += int(chr(content[i]))
+                i += 1
+            resultado += resultado_aux
+        else:
+            i += 1
+
+def main(argv):
+    if len(argv) != 2:
+        print("Utilize: python(3) tpc3.py nome_do_ficheiro")
+        sys.exit(1)
+
+    somador_on_off(argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv)
